3892baad9d43385001bfc582ac16765fc5eb23af
Brian Kim <brian.kyounghoon.kim@gmail.com>
2021-07-11 22:08:07 -0400

    refactor functions

--- script.js
+++ script.js
@@ -1,13 +1,55 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById("nav");
+const toggleIcon = document.getElementById("toggle-icon");
+const image1 = document.getElementById("image1");
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
+const textBox = document.getElementById("text-box");
+
+// Dark or Light Images
+const imageMode = (color) => {
+  image1.src = `/image/pics/online_${color}.svg`;
+  image2.src = `/image/pics/web_${color}.svg`;
+  image3.src = `/image/pics/work_${color}.svg`;
+};
+
+const toggleDarkLightMode = (isDark) => {
+  nav.style.background = isDark
+    ? "rgba(0, 0, 0, 0.5)"
+    : "rgba(255, 255, 255, 0.5)";
+  textBox.style.backgroundColor = isDark
+    ? "rgba(255, 255, 255, 0.5)"
+    : "rgba(0, 0, 0, 0.5)";
+  toggleIcon.children[0].textContent = isDark ? "Dark Mode" : "Light Mode";
+  isDark
+    ? toggleIcon.children[1].classList.replace("fa-sun", "fa-moon")
+    : toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+  isDark ? imageMode("dark") : imageMode("light");
+};
 
 // Switch Theme Function
 const switchTheme = (e) => {
   if (e.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+    toggleDarkLightMode(true);
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+    toggleDarkLightMode(false);
   }
 };
 
 // Event Listener
 toggleSwitch.addEventListener("change", switchTheme);
+
+// check local storage for theme
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+    toggleDarkLightMode(true);
+  }
+}
